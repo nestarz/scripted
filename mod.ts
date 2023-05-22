e8b5dd5b1e5bcf68aeac20f4c789d84cb80d7dc8
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2023-05-22 10:17:37 +0200

    feat: renaming and simple execution storage

--- mod.ts
+++ mod.ts
@@ -8,30 +8,36 @@ const scriptStore: [Map<string, string>, Map<string, string>] = [
   new Map(),
 ];
 
-export const cleanShalimar = () => scriptStore.forEach((map) => map.clear());
+export const clearScriptStore = () => scriptStore.forEach((map) => map.clear());
 
-export const scriptedGlobals = (globals: Record<string, unknown> = {}): void =>
+export const storeGlobalValues = (
+  globals: Record<string, unknown> = {}
+): void => {
   Object.entries(globals)
     .map(([k, v]) => [
       k,
       typeof v === "function" ? v.toString() : JSON.stringify(v),
     ])
     .map(([k, v]) => scriptStore[0].set(k, `const ${k} = ${v};`));
+};
 
-export const scriptedGet = (): string =>
+export const collectScriptContents = (): string =>
   scriptStore
     .flatMap((d) => d.values())
     .map((d) => [...d])
     .flat()
     .join("\n");
-    
-export const scriptedGetClean = (): string => {
-  const res = scriptedGet();
-  cleanShalimar();
-  return res;
-}
 
-export const scripted = (fn: Function, ...args: any[]): string => {
+export const collectAndCleanScripts = (): string => {
+  const res = collectScriptContents();
+  clearScriptStore();
+  return res;
+};
+
+export const storeFunctionWithArgs = (
+  fn: Function,
+  ...args: any[]
+): [string, string, string[]] => {
   const setFn = (fn: Function): string => {
     const id = `_${getHashSync(fn.toString())}`;
     scriptStore[1].set(id, `const fn${id} = ${fn.toString()};`);
@@ -47,6 +53,25 @@ export const scripted = (fn: Function, ...args: any[]): string => {
   );
   const argsId = getHashSync(argsStrArr.join(""));
   const elId = [id, argsId].join("_");
+
+  scriptStore[1].set(elId, `fn${id}(${argsStrArr.join(", ")});`);
+  return [id, elId, argsStrArr];
+};
+
+export const storeFunctionExecution = (
+  fn: Function,
+  ...args: any[]
+): string => {
+  const [id, elId, argsStrArr] = storeFunctionWithArgs(fn, ...args);
+  scriptStore[1].set(elId, `fn${id}(${argsStrArr.join(", ")});`);
+  return elId;
+};
+
+export const scripted = (
+  fn: Function,
+  ...args: any[]
+): string => {
+  const [id, elId, argsStrArr] = storeFunctionWithArgs(fn, ...args);
   scriptStore[1].set(
     elId,
     `document.querySelectorAll(".${elId}").forEach(${
